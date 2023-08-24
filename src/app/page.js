94d9e9e879b9b2c7b28e675cc566801abbe9a1d6
Bryan Garcia <bryan.garcia.cuevas@gmail.com>
2023-08-23 23:33:40 -0700

    routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,20 @@
+'use client'
 import React from "react"
 import BottomNavbar from "./components/BottomNavbar"
 import TopNavBar from "./components/TopNavBar"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 export default function Home() {
   return (
     <>
       <TopNavBar/>
-      <div className='body'>
-        body
-      </div>
+        <Router>
+          <Routes>
+            <Route path="/home">
+
+            </Route>
+          </Routes>
+        </Router>
       <BottomNavbar/>
     </>
   )
